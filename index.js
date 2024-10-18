3bee782fe824194b67fee7c4a4839f08835de050
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-10-19 01:32:06 +0300

    The Lost Context Bug Code-Along

--- index.js
+++ index.js
@@ -2,11 +2,74 @@ const messageConfig = {
   frontContent: "Happy Birthday, Odin One-Eye!",
   insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
   closing: {
-      "Thor": "Admiration, respect, and love",
-      "Loki": "Your son"
+    "Thor": "Admiration, respect, and love",
+    "Loki": "Your son"
   },
   signatories: [
-      "Thor",
-      "Loki"
+    "Thor",
+    "Loki"
   ]
 };
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  const contextBoundForEachExpr = function (signatory) {
+    const message = `${this.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  }.bind(this);
+
+  this.signatories.forEach(contextBoundForEachExpr);
+};
+
+printCard.call(messageConfig);
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  const outerContext = this;
+
+  this.signatories.forEach(function (signatory) {
+    const message = `${outerContext.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  });
+};
+
+printCard.call(messageConfig);
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+  // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
+/* OUTPUT:
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
